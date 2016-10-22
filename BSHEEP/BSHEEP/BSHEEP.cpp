49166e00a323228661811ecd918cc3252ebded3e
joronia <jaoronia@gmail.com>
2016-10-21 18:05:14 -0700

    Input implementation
    Input is working fine.

--- BSHEEP/BSHEEP/BSHEEP.cpp
+++ BSHEEP/BSHEEP/BSHEEP.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-
+	int x, y = 0;
+	int counter = 0;
+	int num_tests = 0;
+	int num_of_sheep = 0;
+	cin >> num_tests;
+	cout << endl;
+	
+	
+	while (counter != num_tests) {
+		cin >> num_of_sheep;
+		for (int i = 0; i < num_of_sheep; i++) {
+			cin >> x >> y;
+		}
+		cout << endl;
+		counter++;
+	}
 }
